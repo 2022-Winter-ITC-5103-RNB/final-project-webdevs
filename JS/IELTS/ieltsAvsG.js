9d13e49c036c7a693b873dd7ed394c56f17961e1
rudresh74 <rjojasani@gmail.com>
2022-04-12 12:12:49 -0400

    master latest

--- JS/IELTS/ieltsAvsG.js
+++ JS/IELTS/ieltsAvsG.js
@@ -4,6 +4,8 @@ document.getElementById('button').addEventListener("click", function() {
 
 document.querySelector('.close').addEventListener("click", function() {
 	document.querySelector('.bg-modal').style.display = "none";
+	document.querySelector('#name').value='';
+	document.querySelector('#email').value='';
 });
 
 document.querySelector('.subClose').addEventListener("click", function() {
@@ -11,7 +13,39 @@ document.querySelector('.subClose').addEventListener("click", function() {
 });
 
 document.getElementById("formSubmit").addEventListener("click", function(){
-	document.querySelector('.bg-modal').style.display = "none";
-	document.querySelector('.after-submitting').style.display="flex";
+
+	let name= document.querySelector('#name');
+	let namePattern=/^[a-zA-z]{3,30}$/;
+	let email=document.querySelector('#email');
+	let emailPattern=/^\S+@\S+\.\S+$/;
+
+	if(name.value==''){
+		document.querySelector('#name').placeholder='Please enter name';
+		name.classList.add('nameValidation');
+	}
+	else if(!namePattern.test(name.value)){
+		document.querySelector('#name').value='';
+		document.querySelector('#name').placeholder='Please enter valid name';
+		name.classList.add('nameValidation');
+	}
+	else if(email.value==''){
+		document.querySelector('#email').placeholder='Please enter email';
+		email.classList.add('emailValidation');
+		name.classList.remove('nameValidation');
+	}
+	else if(!emailPattern.test(email.value)){
+		document.querySelector('#email').value='';
+		document.querySelector('#email').placeholder='Please enter valid email';
+		email.classList.add('emailValidation');
+		name.classList.remove('nameValidation');
+	}
+	else{
+		name.classList.remove('nameValidation');
+		email.classList.remove('emailValidation');
+		document.querySelector('.bg-modal').style.display = "none";
+		document.querySelector('.after-submitting').style.display="flex";
+		document.querySelector('#name').value='';
+		document.querySelector('#email').value='';
+	}
 });
 
